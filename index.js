9a67db828b1b9b38f521d1492b4f5784e86b8cb2
sifatullahsu <personal.sifat@gmail.com>
2024-03-14 10:09:29 +0600

    updated video v2

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ app.use(cors());
 
 const mediaRoutes = require("./routes/media");
 const vimeoRoutes = require("./routes/vimeo");
+const vimeoV2Routes = require("./routes/newVimeo");
 
-app.use("/api/v1/media", mediaRoutes); 
+app.use("/api/v1/media", mediaRoutes);
 
-app.use("/api/v1/vimeo", vimeoRoutes); 
+app.use("/api/v1/vimeo", vimeoRoutes);
+app.use("/api/v2/vimeo", vimeoV2Routes);
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-const mongodbUri = "mongodb+srv://mamtazfreelancer:f7FcczeDomuZ5F3L@cluster0.6ds5s8q.mongodb.net/uploadproject";
+const mongodbUri =
+  "mongodb+srv://mamtazfreelancer:f7FcczeDomuZ5F3L@cluster0.6ds5s8q.mongodb.net/uploadproject";
 
 mongoose.connect(mongodbUri, {
   useNewUrlParser: true,
@@ -24,7 +27,7 @@ mongoose.connect(mongodbUri, {
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongodb...");
 });
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 mongoose.connection.on("error", (err) => {
   console.log("Error connecting to mongo", err);
